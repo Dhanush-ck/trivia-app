8ac9c980f232077372e638e638d4fbcc839c57b0
Dhanush CK <dhanushck01@gmail.com>
2025-05-22 01:28:29 +0530

    feat : Registered new model

--- main/admin.py
+++ main/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from . models import Question
+from . models import Profile
 
 # Register your models here.
 
 admin.site.register(Question)
+admin.site.register(Profile)
