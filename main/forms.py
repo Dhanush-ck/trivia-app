d65d2da69e4e470a24a75dc6543247a942970e81
Dhanush CK <dhanushck01@gmail.com>
2025-05-22 01:32:06 +0530

    feat : Created new form and added custom errors

--- main/forms.py
+++ main/forms.py
@@ -1,4 +1,28 @@
 from django import forms
+from django.contrib.auth.models import User
 
 class NameForm(forms.Form):
     name = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'placeholder': 'Enter your name'}))
+    password = forms.CharField(widget=forms.PasswordInput())
+
+    def clean_name(self):
+        name = self.cleaned_data['name']
+        if User.objects.filter(username=name).exists():
+            raise forms.ValidationError('This username already exists')
+        return name
+    
+class UserForm(forms.Form):
+    name = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'placeholder': 'Enter your name'}))
+    password = forms.CharField(widget=forms.PasswordInput())
+
+    def clean(self):
+        cleaned_data = super().clean()
+        name = cleaned_data.get('name')
+        password = cleaned_data.get('password')
+        if User.objects.filter(username=name).exists():
+                user = User.objects.get(username=name)
+                if not user.check_password(password):
+                    raise forms.ValidationError('Password error')
+        else:
+            raise forms.ValidationError('User not found')
+        return cleaned_data
